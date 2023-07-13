284425fa63ec0d7b2beac6c5278f909447859f7f
徐瑞熙 <xu.ruixi@iwhalecloud.com>
2023-07-13 10:01:57 +0800

    update

--- config/prod.config.js
+++ config/prod.config.js
@@ -10,6 +10,19 @@ module.exports = {
     // publicPath: '',
     assetModuleFilename: 'resources/[name].[hash:5][ext]'
   },
+  // 代码分割
+  optimization: {
+    splitChunks: {
+      /*
+          有效值为 `all`，`async` 和 `initial`
+          三种值的区别：
+          all: 所有引入的库都会进行代码分割
+          async: 异步引入的库进行代码分割，（默认）
+          initial: 同步引入的库进行代码分割
+          */
+      chunks: "all",
+    },
+  },
   plugins: [
     // **/*表示会取output.path的目录
     // !取反，表示不会被删除
